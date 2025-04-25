10f31ed0fddff1f33f58479a902888f1ee99048a
Gguife <gguife747@gmail.com>
2025-04-25 01:17:03 -0300

    feat: create currency api client request and create object for conversion rate value

---
+++ src/main/java/com/currency/model/response/ExchangeRateResponse.java
@@ -0,0 +1,9 @@
+package com.currency.model.response;
+
+public class ExchangeRateResponse {
+    private double conversion_rate;
+
+    public double getConversion_rate() {
+        return conversion_rate;
+    }
+}
